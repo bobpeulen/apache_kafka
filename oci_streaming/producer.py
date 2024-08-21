9a7732a902b61825e11350f0860a283cdb0ea2df
Bob Peulen <75362416+bobpeulen@users.noreply.github.com>
2024-08-21 16:59:00 +0200

    Update producer.py

--- oci_streaming/producer.py
+++ oci_streaming/producer.py
@@ -1,5 +1,12 @@
 from kafka3 import KafkaConsumer, KafkaProducer
-
+import pandas as pd
+from time import sleep
+import datetime
+import numpy as np
+from datetime import datetime, timedelta
+import random
+import argparse
+import uuid
 
 #get arguments
 parser = argparse.ArgumentParser()
@@ -10,9 +17,6 @@ parser.add_argument("-user_name", "--user_name", help="user_name including Oracl
 parser.add_argument("-stream_name", "--stream_name", help="stream_name / topic", required=True)
 parser.add_argument("-stream_pool_ocid", "--stream_pool_ocid", help="stream_pool_ocid", required=True)
 parser.add_argument("-auth_token", "--auth_token", help="auth_token", required=True)
-parser.add_argument("-batch_size", "--batch_size", help="Set the batch size. Number of rows you want to produce and publish for each publish", required=True)
-parser.add_argument("-batch_interval", "--batch_interval", help="set the batch interval. Number of seconds you want the loop to wait untill next publish.", required=True)
-parser.add_argument("-max_number_of_rows", "--max_number_of_rows", help="set the maximum number of rows you want to publish", required=True)
 
 
 args = parser.parse_args()
@@ -22,27 +26,24 @@ user_name = args.user_name
 stream_name = args.stream_name
 stream_pool_ocid = args.stream_pool_ocid
 auth_token = args.auth_token
-batch_size = args.batch_size
-batch_interval = args.batch_interval
-max_number_of_rows = args.max_number_of_rows
 
 
 def main():
   ## create connection
-  producer = KafkaProducer(bootstrap_servers = f'cell-1.streaming.{region}.oci.oraclecloud.com:9092', linger_ms = 50, batch_size  = batch_size,
+    producer = KafkaProducer(bootstrap_servers = f'cell-1.streaming.{region}.oci.oraclecloud.com:9092', linger_ms = 50, batch_size  = 2,
                          security_protocol = 'SASL_SSL', sasl_mechanism = 'PLAIN',
                          value_serializer = lambda v: v.encode('utf-8'),
                          sasl_plain_username = f'{tenancy_name}/{user_name}/{stream_pool_ocid}',
                          sasl_plain_password = auth_token)
   #create random data
-  pd_list = []
-
-    for i in range(60):
+    pd_list = []
+    
+    for i in range(500):
 
         #set randomness, so each goes up and down together
         randomness_sun_price = random.uniform(0.95, 1.015)
         randomness_wspd = random.uniform(0.90, 0.95)
-        cust_id = fake.uuid4()
+        cust_id = str(uuid.uuid4())
         kwh_producing = round(random.uniform(0.11, 5.55), 2)
         kwh_consuming = round(random.uniform(3.55, 19.55), 2)
 
@@ -53,7 +54,7 @@ def main():
         energy_price = randomness_sun_price *0.89
 
         pd_list.append([cust_id, kwh_producing, kwh_consuming, temp, wspd, tsun, energy_price])
-                     
+
     df_out = pd.DataFrame(pd_list, columns = ["cust_id", "kwh_producing", "kwh_consuming", "temp", "windspeed","sun_light", "energy_price"])
 
   #push random data to stream
@@ -61,8 +62,8 @@ def main():
         row_json = row.to_json(orient='records', lines=True)
         producer.send(stream_name, value=row_json)
         producer.flush()
-        print(row_json)
-        sleep(batch_interval)
+        print(f"Producing to stream: {row_json}")
+        sleep(5)
 
 
 if __name__ == '__main__':
