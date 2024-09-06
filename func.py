82c08ca3df0685c82bd981d6ca3667ee5275f93e
Bob Peulen <75362416+bobpeulen@users.noreply.github.com>
2024-09-06 15:51:53 +0200

    Update func.py

--- func.py
+++ func.py
@@ -4,7 +4,8 @@ import oci
 import logging
 from urllib.parse import urlparse, parse_qs
 from fdk import response
-
+import pandas as pd
+import numpy
 
 def handler(ctx, data: io.BytesIO=None):
     logging.getLogger().info("function handler start")
@@ -25,6 +26,16 @@ def handler(ctx, data: io.BytesIO=None):
         if requestbody_str:
             body = json.loads(requestbody_str)
             print(body)
+            timex = body['payload'][0]['time']
+            z = body['payload'][0]['values']['z']
+            y = body['payload'][0]['values']['y']
+            x = body['payload'][0]['values']['x']
+
+            df = pd.DataFrame([[timex, x, y, z]], columns=["timex", "x", "y", "z"])
+
+            fullname = f"oci://test_sensor_logger@fro8fl9kuqli/output_{timex}.csv"
+            df.to_csv(fullname, index = False)
+        
         else:
             body = {}
     except Exception as ex:
